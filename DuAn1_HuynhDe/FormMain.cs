f291ef0269638d673ae5d32953b5ef915cc7f085
LamSamChan <chan94170@gmail.com>
2022-11-11 22:05:51 +0700

    12312312

--- DuAn1_HuynhDe/FormMain.cs
+++ DuAn1_HuynhDe/FormMain.cs
@@ -20,32 +20,7 @@ namespace DuAn1_HuynhDe
         public FormMain()
         {
             InitializeComponent();
-            //resize window
-            this.SetStyle(ControlStyles.ResizeRedraw,true);
-        }
-        //resize window
-        private const int cGrip = 16;
-        private const int cCaption = 32;
-
-        protected override void WndProc(ref Message m)
-        {
-            if (m.Msg == 0x84)
-            {
-                Point pos = new Point(m.LParam.ToInt32());
-                pos = this.PointToClient(pos);
-
-                if (pos.Y < cCaption)
-                {
-                    m.Result = (IntPtr)2;
-                    return;
-                }
-                if (pos.X >= this.ClientSize.Width - cGrip && pos.Y >= this.ClientSize.Height - cGrip)
-                {
-                    m.Result = (IntPtr)17;
-                    return;
-                }
-            }
-            base.WndProc(ref m);
+            
         }
 
         private IconButton currentBtn;
